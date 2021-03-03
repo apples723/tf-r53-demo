93e6a8e17c06a19aa17d66efe50b3b8be99d3f5e
Grant Siders <gsiders@2u.com>
2021-03-03 10:20:36 -0700

    web03 remote state

--- route53/remote.tf
+++ route53/remote.tf
@@ -6,11 +6,11 @@ data  "terraform_remote_state" "web01" {
         region = "us-east-1"
     }
 }
-data  "terraform_remote_state" "web02" {
+data  "terraform_remote_state" "web03" {
     backend = "s3"
     config = { 
         bucket = "2u-terraform"
-        key = "it/sandbox/r53_demo/web02.tfstate"
+        key = "it/sandbox/r53_demo/web03.tfstate"
         region = "us-east-1"
     }
 }
