d5d25280aad47d3edf33ad6824760defc4c4e0a2
Grant Siders <gsiders@2u.com>
2021-03-03 08:29:41 -0700

    add web02 a reccord

--- route53/remote.tf
+++ route53/remote.tf
@@ -6,3 +6,11 @@ data  "terraform_remote_state" "web01" {
         region = "us-east-1"
     }
 }
+data  "terraform_remote_state" "web02" {
+    backend = "s3"
+    config = { 
+        bucket = "2u-terraform"
+        key = "it/sandbox/r53_demo/web02.tfstate"
+        region = "us-east-1"
+    }
+}
